0be6e501bbe3d633ac4c4f3d1e0be0e00292e79e
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-04-08 01:19:39 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,6 @@
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
-    render json: {message: ''}
+    render json: {message: 'Hashes of data will get converted to JSON'}
   end
 end
